76c25e087741ec6634d7db3a75f562b61c2fabb5
animalx solutions <xsolutionsonline@gmail.com>
2021-02-27 19:47:49 -0500

    maquetacion del dashboard

--- src/app/core/shared/shared.module.ts
+++ src/app/core/shared/shared.module.ts
@@ -1,14 +1,18 @@
 import { NgModule } from '@angular/core';
 import { HttpClientModule } from '@angular/common/http';
 import { RouterModule } from '@angular/router';
-
+import {MatGridListModule} from '@angular/material/grid-list';
 import { NotFoundComponent } from './components/not-found/not-found.component';
-
+import {CommonModule} from '@angular/common';
+import { FlexLayoutModule } from '@angular/flex-layout';
+import {MatIconModule} from '@angular/material/icon';
+import {MatButtonModule} from '@angular/material/button';
+import {MatCardModule} from '@angular/material/card';
 
 @NgModule({
-  imports: [HttpClientModule, RouterModule],
+  imports: [HttpClientModule, RouterModule,MatGridListModule,CommonModule,FlexLayoutModule,MatIconModule,MatButtonModule,MatCardModule],
   declarations: [NotFoundComponent],
-  exports: [HttpClientModule, RouterModule, NotFoundComponent],
+  exports: [HttpClientModule, RouterModule, NotFoundComponent,MatGridListModule,CommonModule,FlexLayoutModule,MatIconModule,MatButtonModule,MatCardModule],
   providers: [],
 })
 export class SharedModule {
